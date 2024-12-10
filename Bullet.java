ce3fa68a79a88da9db2ec5b734a0a7a397a96555
Jenyyk <jendotron2@gmail.com>
2024-12-10 10:52:16 +0100

    refactor: removed unneccesary Greenfoot comments

--- Bullet.java
+++ Bullet.java
@@ -1,11 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Bullet here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Bullet extends Projectile
 {
     public Bullet(Enemy enemy, float damage, int spawnX, int spawnY) {
